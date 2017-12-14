11879b0cab4e08b8f6dbfd9d2d3afd27e93bc4a1
anahostinger <30921896+anahostinger@users.noreply.github.com>
2017-12-14 18:04:03 +0700

    Add files via upload

---
+++ www/assets/script.js
@@ -0,0 +1,31 @@
+var url = "http://localhost/_projects-apps/5RA/adminsystem";
+function getScript( urlTarget = url + "/apps.js", onSuccess ){
+	$.ajax({
+		url: urlTarget,
+		dataType: 'script',
+		error: ajax_err,
+		success:onSuccess,
+		async: true
+	}); 
+}
+function tryAgain(msg){ 
+	alert(msg); 
+	if(confirm('Coba kembali ?')){ 
+		getScript(url); 
+	}else{
+		$("#loading").hide();
+		$("#logo").removeClass("hide");
+	}
+}
+function ajax_err(jqXHR, exception){
+	if (jqXHR.status === 0) { tryAgain('Tidak ada koneksi\nPastikan jaringan anda aktif');} 
+	else if (jqXHR.status == 404) { tryAgain('Halaman server tidak ditemukan [404]'); } 
+	else if (jqXHR.status == 500) { tryAgain('Internal Server Error [500]'); } 
+	else if (exception === 'parsererror') { tryAgain('Gagal pembacaan JSON yang dibutuhkan'); } 
+	else if (exception === 'timeout') { tryAgain('Terlalu lama'); } 
+	else if (exception === 'abort') { tryAgain('Koneksi ditolak'); } 
+	else { tryAgain('Error tidak biasa :\n' + jqXHR.responseText); }
+}
+$(document).ready(function(){
+	getScript(url + "/apps.js");
+});
